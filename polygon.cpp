733d066ba2df0e4fe262045a71d57749b4a7b76c
Kobaysh <73707817+Kobaysh@users.noreply.github.com>
2021-05-26 16:47:56 +0900

    3DCube

--- polygon.cpp
+++ polygon.cpp
@@ -46,106 +46,209 @@ CPolygon::~CPolygon()
 }
 
  void CPolygon::Draw()
-{
-	// ライトオフ
-	glDisable(GL_LIGHTING);
+ {
+	 // ライトオフ
+	 glDisable(GL_LIGHTING);
 
-	CRenderer::OpenGLSet2D();
-	// プロジェクション行列->2D用の行列がセットされた状態
-	// モデルビュー行列->単位行列がセットされた状態
-	
-	// テクスチャのセット
-	glBindTexture(GL_TEXTURE_2D, m_Texture);
-	const int n = PYRAMID_NUM;
-	static float rotZ = 0.0f;	// 回転角度z
-	// 表示座標
-	static VECTOR3D Position1(SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f, -0);
-	
+	 CRenderer::OpenGLSet2D();
+	 // プロジェクション行列->2D用の行列がセットされた状態
+	 // モデルビュー行列->単位行列がセットされた状態
 
-	VECTOR3D PositionPyramid;
-	
-	// 2dポリゴン表示
-	// 回転ポリゴンを表示
-	//for (int i = 0; i < n; i++)
-	//{
-	//	glMatrixMode(GL_MODELVIEW);	// ポリゴン描画行列の操作に切り替える
-	//	glPushMatrix();				// 現在のモデルビュー行列を保存
-	//	{
-	//		// ピラミッド位置
-	//		glTranslatef(Position1.x, Position1.y, Position1.z);
-	//		glRotatef(rotZ, 0, 0, 1.0f);	// 平行移動行列に乗算
-	//		rotZ += 0.1f;
-	//	//	glScalef(1.0f, 1.0f, 1.0f);
-	//		// 平行移動行列
-	//		glTranslatef(Positions[i].x, Positions[i].y, Positions[i].z);	// スタックに乗算
-	//		// 回転行列
-	//	
-
-	//		// ポリゴンセット
-	//		glBegin(GL_TRIANGLE_STRIP);			// 頂点のセット開始
-	//		{
-	//			glColor4f(1.0f, 0.0f, 0.0f, 1.0f);	// 頂点1のカラーセット
-	//			glTexCoord2f(0.0f, 0.0f);			// 頂点1のテクスチャ座標
-	//			glVertex3f(-50.0f, -50.0f, 0.0f);	// 頂点1の画面座標
-
-	//			glColor4f(0.0f, 1.0f, 0.0f, 1.0f);	// 頂点2のカラーセット
-	//			glTexCoord2f(0.0f, -1.0f);			// 頂点2のテクスチャ座標
-	//			glVertex3f(-50.0f, 50.0f, 0.0f);	// 頂点2の画面座標
-
-	//			glColor4f(0.0f, 0.0f, 1.0f, 1.0f);	// 頂点3のカラーセット
-	//			glTexCoord2f(1.0f, 0.0f);			// 頂点3のテクスチャ座標
-	//			glVertex3f(50.0f, -50.0f, 0.0f);	// 頂点3の画面座標
-
-	//			glColor4f(1.0f, 1.0f, 1.0f, 1.0f);	// 頂点4のカラーセット
-	//			glTexCoord2f(1.0f, -1.0f);			// 頂点4のテクスチャ座標
-	//			glVertex3f(50.0f, 50.0f, 0.0f);	// 頂点4の画面座標
+	 // テクスチャのセット
+	 glBindTexture(GL_TEXTURE_2D, m_Texture);
+	 const int n = PYRAMID_NUM;
+	 static float rotZ = 0.0f;	// 回転角度z
+	 // 表示座標
+	 static VECTOR3D Position1(SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f, -0);
 
 
-	//		}
-	//		glEnd();
-	//	}
-	//	glMatrixMode(GL_MODELVIEW);
-	//	glPopMatrix();		// 行列スタックの復帰
-	//
-	//}
+	 VECTOR3D PositionPyramid;
+
+	 // 2dポリゴン表示
+	 // 回転ポリゴンを表示
+	 //for (int i = 0; i < n; i++)
+	 //{
+	 //	glMatrixMode(GL_MODELVIEW);	// ポリゴン描画行列の操作に切り替える
+	 //	glPushMatrix();				// 現在のモデルビュー行列を保存
+	 //	{
+	 //		// ピラミッド位置
+	 //		glTranslatef(Position1.x, Position1.y, Position1.z);
+	 //		glRotatef(rotZ, 0, 0, 1.0f);	// 平行移動行列に乗算
+	 //		rotZ += 0.1f;
+	 //	//	glScalef(1.0f, 1.0f, 1.0f);
+	 //		// 平行移動行列
+	 //		glTranslatef(Positions[i].x, Positions[i].y, Positions[i].z);	// スタックに乗算
+	 //		// 回転行列
+	 //	
+
+	 //		// ポリゴンセット
+	 //		glBegin(GL_TRIANGLE_STRIP);			// 頂点のセット開始
+	 //		{
+	 //			glColor4f(1.0f, 0.0f, 0.0f, 1.0f);	// 頂点1のカラーセット
+	 //			glTexCoord2f(0.0f, 0.0f);			// 頂点1のテクスチャ座標
+	 //			glVertex3f(-50.0f, -50.0f, 0.0f);	// 頂点1の画面座標
+
+	 //			glColor4f(0.0f, 1.0f, 0.0f, 1.0f);	// 頂点2のカラーセット
+	 //			glTexCoord2f(0.0f, -1.0f);			// 頂点2のテクスチャ座標
+	 //			glVertex3f(-50.0f, 50.0f, 0.0f);	// 頂点2の画面座標
+
+	 //			glColor4f(0.0f, 0.0f, 1.0f, 1.0f);	// 頂点3のカラーセット
+	 //			glTexCoord2f(1.0f, 0.0f);			// 頂点3のテクスチャ座標
+	 //			glVertex3f(50.0f, -50.0f, 0.0f);	// 頂点3の画面座標
+
+	 //			glColor4f(1.0f, 1.0f, 1.0f, 1.0f);	// 頂点4のカラーセット
+	 //			glTexCoord2f(1.0f, -1.0f);			// 頂点4のテクスチャ座標
+	 //			glVertex3f(50.0f, 50.0f, 0.0f);	// 頂点4の画面座標
+
+
+	 //		}
+	 //		glEnd();
+	 //	}
+	 //	glMatrixMode(GL_MODELVIEW);
+	 //	glPopMatrix();		// 行列スタックの復帰
+	 //
+	 //}
 
 
 
-	// 3D設定
-	CRenderer::OpenGLSet3D();
-	// カメラの作成->モデルビュー行列に乗算
-	gluLookAt(
-		0.0, 0.0, 10.0,	// カメラ座標
-		0.0, 0.0, 0.0,	// 注視点
-		0.0, 1.0, 0.0);	// カメラの上方向
-	static float rotZ2 = 0.0f;
-	static VECTOR3D Position2(0.0f, 0.0f, 0.0f);
-	glMatrixMode(GL_MODELVIEW);
-	glPushMatrix();
+	 // 3D設定
+	 CRenderer::OpenGLSet3D();
+	 // カメラの作成->モデルビュー行列に乗算
+	 gluLookAt(
+		 0.0, 0.0, 10.0,	// カメラ座標
+		 0.0, 0.0, 0.0,	// 注視点
+		 0.0, 1.0, 0.0);	// カメラの上方向
+	 static float rotZ2 = 0.0f;
+	 static VECTOR3D Position2(0.0f, 0.0f, 0.0f);
+	 glMatrixMode(GL_MODELVIEW);
+	 glPushMatrix();
+	 {
+	 glTranslatef(Position2.x, Position2.y, Position2.z);
+	 glRotatef(rotZ2, 0.4f, 1.0f, 0.5f);
+	 rotZ2 -= 2.0f;
 
-	glTranslatef(Position2.x, Position2.y, Position2.z);
-	glRotatef(rotZ2,0.4f, 1.0f, 0.5f);
-	rotZ2 -= 2.0f;
+	 glBegin(GL_TRIANGLE_STRIP); // ue
+	 {// 上
+		 glColor4f(1.0f, 0.0f, 0.0f, 1.0f);	// 頂点1のカラーセット
+		 glTexCoord2f(0, 0);			// 頂点1のテクスチャ座標
+		 glVertex3f(-1, 1, -1);	// 頂点1の画面座標
 
-	glBegin(GL_TRIANGLE_STRIP);
-	{
-		glColor4f(1.0f, 0.0f, 0.0f, 1.0f);	// 頂点1のカラーセット
-		glTexCoord2f(0, 0);			// 頂点1のテクスチャ座標
-		glVertex3f(-1, 1, 0);	// 頂点1の画面座標
+		 glColor4f(0.0f, 1.0f, 0.0f, 1.0f);	// 頂点2のカラーセット
+		 glTexCoord2f(0, -1);			// 頂点2のテクスチャ座標
+		 glVertex3f(-1, 1, 1);	// 頂点2の画面座標
 
-		glColor4f(0.0f, 1.0f, 0.0f, 1.0f);	// 頂点2のカラーセット
-		glTexCoord2f(0, -1);			// 頂点2のテクスチャ座標
-		glVertex3f(-1, -1, 0);	// 頂点2の画面座標
+		 glColor4f(0.0f, 0.0f, 1.0f, 1.0f);	// 頂点3のカラーセット
+		 glTexCoord2f(1, 0);			// 頂点3のテクスチャ座標
+		 glVertex3f(1, 1, -1);	// 頂点3の画面座標
 
-		glColor4f(0.0f, 0.0f, 1.0f, 1.0f);	// 頂点3のカラーセット
-		glTexCoord2f(1, 0);			// 頂点3のテクスチャ座標
-		glVertex3f(1, 1, 0);	// 頂点3の画面座標
+		 glColor4f(1.0f, 1.0f, 1.0f, 1.0f);	// 頂点4のカラーセット
+		 glTexCoord2f(1, -1);			// 頂点4のテクスチャ座標
+		 glVertex3f(1, 1, 1);	// 頂点4の画面座標
+	 }
+	 glEnd();
 
-		glColor4f(1.0f, 1.0f, 1.0f, 1.0f);	// 頂点4のカラーセット
-		glTexCoord2f(1, -1);			// 頂点4のテクスチャ座標
-		glVertex3f(1, -1, 0);	// 頂点4の画面座標
-	}
-	glEnd();
+	 glBegin(GL_TRIANGLE_STRIP); // shita
+	 {// 下
+		 glColor4f(1.0f, 0.0f, 0.0f, 1.0f);	// 頂点1のカラーセット
+		 glTexCoord2f(0, 0);			// 頂点1のテクスチャ座標
+		 glVertex3f(-1, -1, 1);	// 頂点1の画面座標
+
+		 glColor4f(0.0f, 1.0f, 0.0f, 1.0f);	// 頂点2のカラーセット
+		 glTexCoord2f(0, -1);			// 頂点2のテクスチャ座標
+		 glVertex3f(-1, -1, -1);	// 頂点2の画面座標
+
+		 glColor4f(0.0f, 0.0f, 1.0f, 1.0f);	// 頂点3のカラーセット
+		 glTexCoord2f(1, 0);			// 頂点3のテクスチャ座標
+		 glVertex3f(1, -1, 1);	// 頂点3の画面座標
+
+		 glColor4f(1.0f, 1.0f, 1.0f, 1.0f);	// 頂点4のカラーセット
+		 glTexCoord2f(1, -1);			// 頂点4のテクスチャ座標
+		 glVertex3f(1, -1, -1);	// 頂点4の画面座標
+	 }
+	 glEnd();
+
+	 glBegin(GL_TRIANGLE_STRIP); // shomen
+	 {
+		 glColor4f(1.0f, 0.0f, 0.0f, 1.0f);	// 頂点1のカラーセット
+		 glTexCoord2f(0, 0);			// 頂点1のテクスチャ座標
+		 glVertex3f(-1, 1, 1);	// 頂点1の画面座標
+
+		 glColor4f(0.0f, 1.0f, 0.0f, 1.0f);	// 頂点2のカラーセット
+		 glTexCoord2f(0, -1);			// 頂点2のテクスチャ座標
+		 glVertex3f(-1, -1, 1);	// 頂点2の画面座標
+
+		 glColor4f(0.0f, 0.0f, 1.0f, 1.0f);	// 頂点3のカラーセット
+		 glTexCoord2f(1, 0);			// 頂点3のテクスチャ座標
+		 glVertex3f(1, 1, 1);	// 頂点3の画面座標
+
+		 glColor4f(1.0f, 1.0f, 1.0f, 1.0f);	// 頂点4のカラーセット
+		 glTexCoord2f(1, -1);			// 頂点4のテクスチャ座標
+		 glVertex3f(1, -1, 1);	// 頂点4の画面座標
+	 }
+	 glEnd();
+
+	 glBegin(GL_TRIANGLE_STRIP); // ushiro
+	 {// ushiro
+		 glColor4f(1.0f, 0.0f, 0.0f, 1.0f);	// 頂点1のカラーセット
+		 glTexCoord2f(0, 0);			// 頂点1のテクスチャ座標
+		 glVertex3f(1, 1, -1);	// 頂点1の画面座標
+
+		 glColor4f(0.0f, 1.0f, 0.0f, 1.0f);	// 頂点2のカラーセット
+		 glTexCoord2f(0, -1);			// 頂点2のテクスチャ座標
+		 glVertex3f(1, -1, -1);	// 頂点2の画面座標
+
+		 glColor4f(0.0f, 0.0f, 1.0f, 1.0f);	// 頂点3のカラーセット
+		 glTexCoord2f(1, 0);			// 頂点3のテクスチャ座標
+		 glVertex3f(-1, 1, -1);	// 頂点3の画面座標
+
+		 glColor4f(1.0f, 1.0f, 1.0f, 1.0f);	// 頂点4のカラーセット
+		 glTexCoord2f(1, -1);			// 頂点4のテクスチャ座標
+		 glVertex3f(-1, -1, -1);	// 頂点4の画面座標
+	 }
+	 glEnd();
+
+	 glBegin(GL_TRIANGLE_STRIP); // migi
+	 {// migi
+		 glColor4f(1.0f, 0.0f, 0.0f, 1.0f);	// 頂点1のカラーセット
+		 glTexCoord2f(0, 0);			// 頂点1のテクスチャ座標
+		 glVertex3f(1, 1, 1);	// 頂点1の画面座標
+
+		 glColor4f(0.0f, 1.0f, 0.0f, 1.0f);	// 頂点2のカラーセット
+		 glTexCoord2f(0, -1);			// 頂点2のテクスチャ座標
+		 glVertex3f(1, -1, 1);	// 頂点2の画面座標
+
+		 glColor4f(0.0f, 0.0f, 1.0f, 1.0f);	// 頂点3のカラーセット
+		 glTexCoord2f(1, 0);			// 頂点3のテクスチャ座標
+		 glVertex3f(1, 1, -1);	// 頂点3の画面座標
+
+		 glColor4f(1.0f, 1.0f, 1.0f, 1.0f);	// 頂点4のカラーセット
+		 glTexCoord2f(1, -1);			// 頂点4のテクスチャ座標
+		 glVertex3f(1, -1, -1);	// 頂点4の画面座標
+	 }
+	 glEnd();
+
+	 glBegin(GL_TRIANGLE_STRIP); // hidari
+	 {// hidari
+		 glColor4f(1.0f, 0.0f, 0.0f, 1.0f);	// 頂点1のカラーセット
+		 glTexCoord2f(0, 0);			// 頂点1のテクスチャ座標
+		 glVertex3f(-1, 1, -1);	// 頂点1の画面座標
+
+		 glColor4f(0.0f, 1.0f, 0.0f, 1.0f);	// 頂点2のカラーセット
+		 glTexCoord2f(0, -1);			// 頂点2のテクスチャ座標
+		 glVertex3f(-1, -1, -1);	// 頂点2の画面座標
+
+		 glColor4f(0.0f, 0.0f, 1.0f, 1.0f);	// 頂点3のカラーセット
+		 glTexCoord2f(1, 0);			// 頂点3のテクスチャ座標
+		 glVertex3f(-1, 1, 1);	// 頂点3の画面座標
+
+		 glColor4f(1.0f, 1.0f, 1.0f, 1.0f);	// 頂点4のカラーセット
+		 glTexCoord2f(1, -1);			// 頂点4のテクスチャ座標
+		 glVertex3f(-1, -1, 1);	// 頂点4の画面座標
+	 }
+	 glEnd();
+
+	 }
+
 	glMatrixMode(GL_MODELVIEW);
 	glPopMatrix();
 
